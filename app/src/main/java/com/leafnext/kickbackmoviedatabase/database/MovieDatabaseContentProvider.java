ea63c0e7a9e8ad582d52a3f5c942ea9ff0ffe8c1
Syed Ehteshamuddin <ehteshamuddin.syed@gmail.com>
2018-03-02 16:54:58 -0600

    Created contentprovider shell and also added it to manifest file

---
+++ app/src/main/java/com/leafnext/kickbackmoviedatabase/database/MovieDatabaseContentProvider.java
@@ -0,0 +1,56 @@
+package com.leafnext.kickbackmoviedatabase.database;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by ehteshs1 on 2018/03/02.
+ */
+
+public class MovieDatabaseContentProvider extends ContentProvider{
+
+    private MovieDatabaseHelper mMovieDatabaseHelper;
+    @Override
+    public boolean onCreate() {
+
+
+        Context context = getContext();
+        mMovieDatabaseHelper = new MovieDatabaseHelper(context);
+
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        return null;
+    }
+
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+}
